205a826f4b98af96701d8f693664cf7ea1ae3ce8
dariel0514 <dariel0514@hotmail.com>
2022-09-18 17:14:09 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -4,8 +4,22 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+const poker = document.querySelectorAll(".poker");
+const icon = document.querySelectorAll(".icon");
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const chooseSymbol = Math.floor(Math.random() * 4);
+  const chooseCard = Math.floor(Math.random() * 13);
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Q", "J", "K"];
+  const symbols = ["♥", "♠️", "♦️", "♣️"];
+  let color;
+  if (chooseSymbol === 0 || chooseSymbol === 2) color = "red";
+  else color = "black";
+  poker.forEach(element => {
+    element.innerHTML = cards[chooseCard];
+    element.style.color = color;
+  });
+  icon.forEach(element => {
+    element.innerHTML = symbols[chooseSymbol];
+    element.style.color = color;
+  });
 };
